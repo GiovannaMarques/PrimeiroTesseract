51149352dd1900df160347b9efab33d762805765
Giovanna Marques <79415646+GiovannaMarques@users.noreply.github.com>
2021-11-16 10:18:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Tesseract;
+
+namespace TesteTesseract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--> Start");
+
+            for(int i = 1; i < 19; i++)
+            {
+                var tesseract = new TesseractEngine("C:\\TesteTesseract\\tessdata", "por", EngineMode.Default);
+                tesseract.SetVariable("load_system_dawg", true);
+                tesseract.SetVariable("load_freq_dawg", true);
+                tesseract.SetVariable("tessedit_write_imag", true);
+                var pix = Pix.LoadFromFile($"C:\\TesteTesseract\\TESTE{i}.png");
+                var page = tesseract.Process(pix);
+                var result = page.GetText();
+                Console.WriteLine($"--> TESTE{i}.png");
+                Console.WriteLine($"--> {page.GetMeanConfidence()}");
+                Console.WriteLine($"--> {result}");
+                Console.WriteLine($"-----------------------");
+                Console.WriteLine($"");
+
+            }
+            Console.ReadLine();
+            
+
+            //Cestaoteste.png
+
+            //C:\TesteTesseract\Cestaoteste.png
+        }
+    }
+}
